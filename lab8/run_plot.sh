6762bca982bb4dfb815106eb9bd950282757b006
MarkusMagie <m.sorokin3@g.nsu.ru>
2024-11-17 20:59:09 +0700

    for l2 second test

--- lab8/run_plot.sh
+++ lab8/run_plot.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 g++ -O1 main2.cpp -o main2.out
-time(./main2.out > log.txt)
+time(./main2.out > random_l2_2.txt)
 
 gnuplot <<EOF
 set terminal png
-set output 'plot.png'
-plot 'log.txt' with lines linecolor rgb "red" title 'Random access'
-set output
+set output 'random_l2_2.png'
+
+set arrow from 48, graph 0 to 48, graph 1 nohead lw 2 lc rgb "blue"
+set arrow from 1280, graph 0 to 1280, graph 1 nohead lw 2 lc rgb "blue" 
+
+plot 'random_l2_2.txt' with lines linecolor rgb "red" title 'Random access'
 EOF
 
-echo "Plot saved as plot.png"
+echo "Plot saved as random_l2_2.png"
